8326d82b76df5c71803d5de0d7830fd9befee738
“Myra <myra.santos@curaesoft.com>
2024-09-07 17:03:21 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const todos = [
+  {
+    text: 'have a break and play Uke',
+    isCompleted: false,
+  },
+  {
+    text: 'get back to learning again',
+    isCompleted: false,
+  },
+  {
+    text: 'meet friend for lunch',
+    isCompleted: false,
+  },
+  {
+    text: 'get back to learning again', // Corrected typo
+    isCompleted: false,
+  },
+  {
+    text: 'build todo app',
+    isCompleted: false,
+  },
+  {
+    text: 'show my new app to my kids',
+    isCompleted: false,
+  }
+];
